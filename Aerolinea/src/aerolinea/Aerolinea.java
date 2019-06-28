58ecc4bb06afa15cf6ae6d1ff9cb2a53ad8f084a
CDCSarjento <sebastian_cdc@hotmail.com>
2019-06-27 21:05:03 -0500

    update de lo que trabajo brayan

--- Aerolinea/src/aerolinea/Aerolinea.java
+++ Aerolinea/src/aerolinea/Aerolinea.java
@@ -7,43 +7,156 @@ package aerolinea;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Scanner;
+import javax.swing.JFrame;
 
 /**
  *
  * @author braya
  */
-public class Aerolinea {
-    
+public class Aerolinea extends JFrame {
+
     Integer num_vuelos;
-    
+    int op = -1;
+    String nombreCiudad, ciudadOrigen, ciudadDestino;
+    int eliminaIdCiudad, eliminaIdVuelo;
+    Scanner sc;
     /*Ciudades de las que dispone la aerolínea*/
-    
-    ArrayList<Ciudad> ciudades = new ArrayList<>();
-    
+    ArrayList<Ciudad> ciudades;
+
     /*Vuelos que tiene la aerolínea*/
-    
-    Hashtable<Integer, Vuelo> idVuelos = new Hashtable<>();
-    
-    public Aerolinea(){
-        
+    Hashtable<Integer, Vuelo> Vuelos;
+
+    public Aerolinea() {
+
         num_vuelos = 0;
-        
+        Vuelos = new Hashtable<>();
+        ciudades = new ArrayList<>();
+        sc = new Scanner(System.in);
+
+        while (op != 7) {
+
+            System.out.println("1. Agregar Ciudad");
+            System.out.println("2. Consultar Ciudades");
+            System.out.println("3. Eliminar Ciudad");
+            System.out.println("4. Agregar Vuelo");
+            System.out.println("5. Consultar Vuelos");
+            System.out.println("6. Eliminar Vuelo");
+            System.out.println("7. Salir\n");
+
+            System.out.println("Ingrese la opción: \n");
+            op = sc.nextInt();
+            sc.nextLine();
+
+            switch (op) {
+
+                case 1:
+                    System.out.println("\nIngrese la ciudad que desea agregar: \n");
+                    nombreCiudad = sc.nextLine();
+                    agregarCiudad(nombreCiudad);
+                    System.out.println("\nCiudad agregada correctamente\n");
+                    break;
+
+                case 2:
+                    consultarCiudades();
+                    break;
+
+                case 3:
+                    System.out.println("\nIngrese el número de la ciudad que desea eliminar: \n");
+                    eliminaIdCiudad = sc.nextInt();
+                    sc.nextLine();
+                    eliminarCiudad(eliminaIdCiudad - 1);
+                    System.out.println("\nCiudad eliminada correctamente\n");
+                    break;
+
+                case 4:
+                    System.out.println("\nIngrese la ciudad de origen del vuelo\n");
+                    ciudadOrigen = sc.nextLine();
+                    System.out.println("\nIngrese la ciudad de destino del vuelo\n");
+                    ciudadDestino = sc.nextLine();
+                    agregarVuelo(ciudadOrigen, ciudadDestino);
+                    System.out.println("\nVuelo agregado correctamente\n");
+                    break;
+
+                case 5:
+                    consultarVuelos();
+                    break;
+
+                case 6:
+                    System.out.println("\nIngrese el número del vuelo que desea eliminar: \n");
+                    eliminaIdVuelo = sc.nextInt();
+                    sc.nextLine();
+                    eliminarVuelo(eliminaIdVuelo - 1);
+                    System.out.println("\nVuelo eliminado correctamente\n");
+                    break;
+
+            }
+
+        }
+
     }
-    
-    public void crearVuelo(){
-        
-        idVuelos.put(num_vuelos, new Vuelo());
-        num_vuelos = num_vuelos++;
-        
+
+    public void agregarVuelo(String ciudadOrigen, String ciudadDestino) {
+
+        int c1 = -1, c2 = -1;
+
+        for (int i = 0; i < ciudades.size(); i++) {
+            if (ciudades.get(i).obtenerNombre().equals(ciudadOrigen) && ciudades.get(i).obtenerNombre().equals(ciudadDestino)) {
+                System.out.println("La ciudad de origen y destino es la misma");
+            } else if (ciudades.get(i).obtenerNombre().equals(ciudadOrigen) || ciudades.get(i).obtenerNombre().equals(ciudadDestino)) {
+                if (ciudades.get(i).obtenerNombre().equals(ciudadOrigen)) {
+                    c1 = i;
+                } else if (ciudades.get(i).obtenerNombre().equals(ciudadDestino)) {
+                    c2 = i;
+                }
+            }
+        }
+
+        if (c1 != -1 && c2 != -1) {
+
+            Vuelos.put(num_vuelos, new Vuelo(ciudades.get(c1), ciudades.get(c2)));
+            num_vuelos = num_vuelos + 1;
+
+        } else {
+
+            System.out.println("NO DEBERÍA ENTRAR");
+
+        }
+
     }
-    
-    public void crearCiudad(String nombreCiudad){
-        
-        ciudades.add(new Ciudad(nombreCiudad));
-        
+
+    public void eliminarVuelo(int idVuelo) {
+
+        Vuelos.remove(idVuelo);
+
     }
-    
-    
-    
-    
+
+    public void agregarCiudad(String nombreCiudad) {
+
+        ciudades.add(new Ciudad(nombreCiudad, 1.6, 1.6));
+
+    }
+
+    public void eliminarCiudad(int idCiudad) {
+
+        ciudades.remove(idCiudad);
+
+    }
+
+    public void consultarCiudades() {
+        System.out.println("");
+        for (int i = 0; i < ciudades.size(); i++) {
+            System.out.println(i + 1 + " " + ciudades.get(i).obtenerNombre());
+        }
+        System.out.println("");
+    }
+
+    public void consultarVuelos() {
+        System.out.println("");
+        for (int i = 0; i < Vuelos.size(); i++) {
+            System.out.println(i + 1 + " " + Vuelos.get(i));
+        }
+        System.out.println("");
+    }
+
 }
--- Aerolinea/src/aerolinea/Vuelo.java
+++ Aerolinea/src/aerolinea/Vuelo.java
@@ -12,70 +12,79 @@ import java.util.Date;
  * @author braya
  */
 public class Vuelo {
+
     String codigo, avion;
     char columna[];
     int filas, capacidad, sillasDisponibles;
     Ciudad ciudadOrigen, ciudadDestino;
     Date fechayHora;
     Vuelo vueloSiguiente, vueloAnterior;
-    
+
+    //Constructor vuelo que recibe la ciudad de origen y la de destino
+    public Vuelo(Ciudad ciudadOrigen, Ciudad ciudadDestino) {
+
+        agregarOrigen(ciudadOrigen);
+        agregarDestino(ciudadDestino);
+
+    }
+
     //Asignaciones y eliminaciones para cada variable respectivamente
-    public void asignarCodigo(String cod){
+    public void asignarCodigo(String cod) {
         this.codigo = cod;
     }
-    
-    public void eliminarCodigo(){
+
+    public void eliminarCodigo() {
         this.codigo = null;
     }
-    
-    public void asignarFechaYHora(Date fecha){
+
+    public void asignarFechaYHora(Date fecha) {
         this.fechayHora = fecha;
     }
-    
-    public void eliminarFechaYHora(){
+
+    public void eliminarFechaYHora() {
         this.fechayHora = null;
     }
-    
-    public void asignarAvion(String av){
+
+    public void asignarAvion(String av) {
         this.avion = av;
     }
-    
-    public void eliminarAvion(){
+
+    public void eliminarAvion() {
         this.avion = null;
     }
-    
-    public void agregarAnterior(Vuelo anterior){
+
+    public void agregarAnterior(Vuelo anterior) {
         this.vueloAnterior = anterior;
     }
-    
-    public void eliminarAnterior(){
+
+    public void eliminarAnterior() {
         this.vueloAnterior = null;
     }
 
-    public void agregarSiguiente(Vuelo siguiente){
+    public void agregarSiguiente(Vuelo siguiente) {
         this.vueloSiguiente = siguiente;
     }
-    
-    public void eliminarSiguiente(){
+
+    public void eliminarSiguiente() {
         this.vueloSiguiente = null;
     }
-    
-    public void agregarOrigen(Ciudad origen){
+
+    public void agregarOrigen(Ciudad origen) {
         this.ciudadOrigen = origen;
     }
-    
-    public void eliminarOrigen(){
+
+    public void eliminarOrigen() {
         this.ciudadOrigen = null;
     }
-    
-    public void agregarDestino(Ciudad destino){
+
+    public void agregarDestino(Ciudad destino) {
         this.ciudadDestino = destino;
     }
-    
-    public void eliminarDestino(){
+
+    public void eliminarDestino() {
         this.ciudadDestino = null;
     }
-    
+
     //Get's para cada variable respectivamente
     public String getCodigo() {
         return codigo;
@@ -120,13 +129,13 @@ public class Vuelo {
     public Vuelo getVueloAnterior() {
         return vueloAnterior;
     }
-    
+
     //Constructor(Datos predet)
-    public Vuelo(){ 
-        columna[0]='A';
-        columna[1]='B';
-        columna[2]='C';
-        columna[3]='D';
-        columna[4]='E';
+    public Vuelo() {
+        columna[0] = 'A';
+        columna[1] = 'B';
+        columna[2] = 'C';
+        columna[3] = 'D';
+        columna[4] = 'E';
     }
 }
